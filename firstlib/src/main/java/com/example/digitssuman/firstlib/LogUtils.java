358d48256dcca629ef844ad5455fd876599bb9f0
Suman Neupane <sumanmyon.sam@gmail.com>
2017-12-22 14:14:10 +0545

    add module

--- firstlib/src/main/java/com/example/digitssuman/firstlib/LogUtils.java
+++ firstlib/src/main/java/com/example/digitssuman/firstlib/LogUtils.java
@@ -1,8 +1,13 @@
 package com.example.digitssuman.firstlib;
 
+import android.util.Log;
+
 /**
  * Created by suman on 12/22/2017.
  */
 
 public class LogUtils {
+    public static void main (String[] args){
+        System.out.println("Hello module");
+    }
 }
